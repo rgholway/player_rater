16ad96c05aa016667b17447b945fcd3afd27ea39
Richard Holway <rgholway@syr.edu>
2019-04-06 15:55:43 -0400

    Added buttons

--- app/javascript/react/components/PositionTile.js
+++ app/javascript/react/components/PositionTile.js
@@ -3,20 +3,39 @@ import React, { Component } from 'react'
 class PositionTile extends Component {
   constructor(props) {
     super(props)
+    this.state = {status: ""}
     this.handleClick = this.handleClick.bind(this)
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
 }
 
 handleClick(){
   this.props.addPlayer("add_player", `${this.props.position}`, `${this.props.id}`)
 }
 
+handleMouseEnter(){
+  this.setState({status: this.props.position})
+}
+
+handleMouseLeave(){
+  this.setState({status: ""})
+}
+
+handleDelete(){
+debugger;
+}
+
 render() {
     return(
       <div>
-        <div className={`${this.props.position}--${this.props.selectedFormation}`} onClick={this.handleClick}>
-          <div className="photo">
-            <div className="plus_player">Add Player</div>
-            <img className="field_photo" type="image" src={this.props.image}></img>
+      <div className={`${this.props.position}--${this.props.selectedFormation}`}>
+          <div className="photo" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+            <div className="plus_player" onClick={this.handleClick}>Add Player</div>
+            <img className="field_photo" type="image" src={this.props.image} onClick={this.handleClick}></img>
+            <div className={`${this.state.status}--delete`}>
+              <button className="delete__style" onClick={this.handleDelete}> X </button>
+            </div>
           </div>
           <div className="info_totw">{this.props.selectedFormation}</div>
           <div className="position_totw">{this.props.playerPosition}</div>
